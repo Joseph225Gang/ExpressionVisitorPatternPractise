d59b19b20dee287d3a0a57e01722288c7ad6926a
Joseph <lai225joseph@gmail.com>
2020-01-06 17:04:46 +0800

    add more delegate sample

---
+++ DelAndGeneric/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DelAndGeneric
+{
+    class Program
+    {
+        public delegate void TypeAction<T>(T param);
+        static void Main(string[] args)
+        {
+            ExecuteAction<string>(StringAction, "Hello");
+            ExecuteAction<DateTime>(DateTimeAction, DateTime.Now);
+            ExecuteAction<int>(IntAction, 1000);
+            ExecuteAction<long>(LongAction, 3000);
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void StringAction(string param)
+        {
+            Console.WriteLine(param + " is string type");
+        }
+
+        public static void LongAction(long param)
+        {
+            Console.WriteLine(param + " is long type");
+        }
+
+        public static void IntAction(int param)
+        {
+            Console.WriteLine(param + " is interger type");
+        }
+
+        public static void DateTimeAction(DateTime param)
+        {
+            Console.WriteLine(param + " is datetime type");
+        }
+
+        public static void ExecuteAction<T>(TypeAction<T> action, T param)
+        {
+            action.Invoke(param);
+        }
+    }
+}
---
+++ DelegateMorePractise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegateMorePractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "櫻桃";//使用者輸入的文字
+
+            Dictionary<string, Action> d = new Dictionary<string, Action>() {
+           {"蘋果",  ()=> Console.WriteLine("Apple") }  , //各條件要做的事....
+           {"香蕉",  ()=> Console.WriteLine("Banana")},
+           {"櫻桃",  ()=>Console.WriteLine("Cherry")  },
+           };
+
+            d[userInput].Invoke();//直接挑選要執行的Method來執行
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
